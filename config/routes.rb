98cff79f68acd314c04978256d3732224bb86739
Tom <tomrhernandez@gmail.com>
2015-08-18 16:59:51 +0000

    Generated Users and Microposts models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
-  root "application#hello"
+  resources :users
+  resources :microposts
+  root "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
